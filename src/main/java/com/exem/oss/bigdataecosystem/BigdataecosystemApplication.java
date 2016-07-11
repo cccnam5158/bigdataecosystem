3dfb3531d6827cd3ce12510efa8cadeccafae594
남종환 <jhnam@ex-em.com>
2016-07-11 21:11:42 +0900

    변경> spring boot 설정 추가 예정> 차후 angularjs 에 에코시스템을 표현한다.

---
+++ src/main/java/com/exem/oss/bigdataecosystem/BigdataecosystemApplication.java
@@ -0,0 +1,12 @@
+package com.exem.oss.bigdataecosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigdataecosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigdataecosystemApplication.class, args);
+	}
+}
